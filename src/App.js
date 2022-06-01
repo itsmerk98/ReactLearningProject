2bef5ff795e22f79d0327869bb4ad383a0a1778a
itsmerk98 <itsmerk98@outlook.com>
2022-06-01 08:39:59 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import ExpenseItem from './components/ExpenseItem.js'
+import Expenses from './components/Expenses.js'
 
 function App() {
+    let expenses = [
+        {
+            id: 'e1',
+            title: 'School Fee',
+            amount: '250',
+            date: new Date(2022, 22, 23)
+        },
+        {
+            id: 'e2',
+            title: 'Book Fee',
+            amount: '230',
+            date: new Date(2022, 22, 23)
+        }, {
+            id: 'e3',
+            title: 'House Rent',
+            amount: '700',
+            date: new Date(2022, 22, 23)
+        },
+        {
+            id: 'e4',
+            title: 'Food',
+            amount: '250',
+            date: new Date(2022, 22, 23)
+        }
+    ];
+    
     return (
-        <>
-            <div className='app-container'>
-                <ExpenseItem />
-            </div>
-        </>
+        <div className='app-container'>
+            <Expenses item={expenses}  />
+        </div>
     );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js'
 
-let tital ="Car Insurence"
 
-let ExpenseItem = () => <div className='expense-item'>
-    <div>March 12th 2021</div>
-    <div className='expense-item__description'>
-        <h2>{tital}</h2>
-        <div className='expense-item__price'>$200</div>
-    </div>
-</div>;
-
+function ExpenseItem(props) {
+    return (
+        <div className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </div>);
+}
 export default ExpenseItem;
